522eab4281b1745f239947772da08303d6a0bf13
ErrorCommander <error.commander.dev@gmail.com>
2024-06-19 01:37:42 +0300

    creating Agents

--- Assets/CodeBase/Infrastructure/StateMachine/State/GameState/GameplayLoopState.cs
+++ Assets/CodeBase/Infrastructure/StateMachine/State/GameState/GameplayLoopState.cs
@@ -1,3 +1,6 @@
+using CodeBase.Gameplay;
+using CodeBase.Infrastructure.Factory;
+
 namespace CodeBase.Infrastructure.StateMachine.State.GameState
 {
   /// <summary>
@@ -6,15 +9,19 @@ namespace CodeBase.Infrastructure.StateMachine.State.GameState
   /// <seealso cref="IState"/>
   public class GameplayLoopState : IState
   {
+    private const int _agentCount = 10;
     private readonly GameStateMachine _stateMachine;
+    private readonly IGameFactory _factory;
 
-    public GameplayLoopState(GameStateMachine stateMachine)
+    public GameplayLoopState(IGameFactory factory, GameStateMachine stateMachine)
     {
       _stateMachine = stateMachine;
+      _factory = factory;
     }
     
     public void Enter()
     {
+      FlockHandler flockHandler = new FlockHandler(_factory, _agentCount);
     }
 
     public void Exit()
